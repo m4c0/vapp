c540f755de9082bc9d5a984951a8f0855f91cab9
Eduardo Costa <m4c0@users.noreply.github.com>
2025-07-11 18:16:48 +0100

    poc: isolate render code

--- poc.cpp
+++ poc.cpp
@@ -6,28 +6,44 @@ import vee;
 import voo;
 import vapp;
 
+class pipeline {
+  voo::one_quad m_oq;
+  vee::pipeline_layout m_pl;
+  vee::render_pass m_rp;
+  vee::gr_pipeline m_gp;
+
+public:
+  explicit pipeline(voo::device_and_queue & dq) :
+    m_oq { dq }
+  , m_pl { vee::create_pipeline_layout() }
+  , m_rp { voo::single_att_render_pass(dq) }
+  , m_gp { vee::create_graphics_pipeline({
+    .pipeline_layout = *m_pl,
+    .render_pass = *m_rp,
+    .shaders {
+      voo::shader("poc.vert.spv").pipeline_vert_stage(),
+      voo::shader("poc.frag.spv").pipeline_frag_stage(),
+    },
+    .bindings { m_oq.vertex_input_bind() },
+    .attributes { m_oq.vertex_attribute(0) },
+  }) }
+  {}
+
+  void run(vee::command_buffer cb, vee::extent e) {
+    vee::cmd_bind_gr_pipeline(cb, *m_gp);
+    vee::cmd_set_viewport(cb, e);
+    vee::cmd_set_scissor(cb, e);
+    m_oq.run(cb, 0);
+  }
+};
+
 struct shrt : public vapp {
   void run() override {
     main_loop("poc-voo", [&](auto & dq, auto & sw) {
-      voo::one_quad oq { dq };
-      auto pl = vee::create_pipeline_layout();
-      auto rp = voo::single_att_render_pass(dq);
-      auto gp = vee::create_graphics_pipeline({
-        .pipeline_layout = *pl,
-        .render_pass = *rp,
-        .shaders {
-          voo::shader("poc.vert.spv").pipeline_vert_stage(),
-          voo::shader("poc.frag.spv").pipeline_frag_stage(),
-        },
-        .bindings { oq.vertex_input_bind() },
-        .attributes { oq.vertex_attribute(0) },
-      });
+      pipeline p { dq };
       render_loop(dq, sw, [&] {
         auto cb = sw.command_buffer();
-        vee::cmd_bind_gr_pipeline(cb, *gp);
-        vee::cmd_set_viewport(cb, sw.extent());
-        vee::cmd_set_scissor(cb, sw.extent());
-        oq.run(cb, 0);
+        p.run(cb, sw.extent());
       });
     });
   }
@@ -36,27 +52,12 @@ struct shrt : public vapp {
 struct lng : public vapp {
   void run() override {
     main_loop("poc-voo", [&](auto & dq, auto & sw) {
-      voo::one_quad oq { dq };
-      auto pl = vee::create_pipeline_layout();
-      auto rp = voo::single_att_render_pass(dq);
-      auto gp = vee::create_graphics_pipeline({
-        .pipeline_layout = *pl,
-        .render_pass = *rp,
-        .shaders {
-          voo::shader("poc.vert.spv").pipeline_vert_stage(),
-          voo::shader("poc.frag.spv").pipeline_frag_stage(),
-        },
-        .bindings { oq.vertex_input_bind() },
-        .attributes { oq.vertex_attribute(0) },
-      });
+      pipeline p { dq };
       extent_loop(dq.queue(), sw, [&] {
         sw.queue_one_time_submit(dq.queue(), [&] {
           auto crp = sw.cmd_render_pass();
           auto cb = sw.command_buffer();
-          vee::cmd_bind_gr_pipeline(cb, *gp);
-          vee::cmd_set_viewport(cb, sw.extent());
-          vee::cmd_set_scissor(cb, sw.extent());
-          oq.run(cb, 0);
+          p.run(cb, sw.extent());
         });
       });
     });
